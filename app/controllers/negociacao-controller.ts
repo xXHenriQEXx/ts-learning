be30d79db9378c85a764e5bd78bb5a7044c61448
Henrique Santos <henriqe.e2@gmail.com>
2022-08-27 00:49:10 -0300

    att1

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,41 @@
+import { Negociacao } from './../models/negociacao.js';
+export class NegociacaoController{
+
+private inputData: HTMLInputElement;
+private inputQuantidade: HTMLInputElement;
+private inputInputValor: HTMLInputElement;
+
+constructor(){
+    this.inputData = document.querySelector('#data');
+    this.inputQuantidade = document.querySelector('#quantidade');
+    this.inputInputValor = document.querySelector('#valor');
+   
+}
+
+adiciona(): void{
+
+    const negociacao = this.criaNegociacao();
+    console.log(negociacao)
+    this.limparFormulario();
+
+
+}
+
+criaNegociacao(): Negociacao{
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ','))
+    const quantidade = parseInt(this.inputQuantidade.value)
+    const valor = parseFloat (this.inputInputValor.value)
+    return new Negociacao(date, quantidade, valor);
+}
+
+limparFormulario(): void{
+
+    this.inputData.value = '';
+    this.inputQuantidade.value = '';
+    this.inputInputValor.value = '';
+    this.inputData.focus();
+
+}
+
+};
---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,27 @@
+import { Negociacao } from './../models/negociacao.js';
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputInputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputInputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputInputValor.value = '';
+        this.inputData.focus();
+    }
+}
+;
